0bc766fa824523eecb084d17b2ffaf74b3af3ada
Ciaran Cairns <ciarancairns@googlemail.com>
2025-01-15 12:35:01 +0000

    Fixed  app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,7 +6,7 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/employees');
+var employeesRouter = require('./routes/employees'); // Renamed
 
 var app = express();
 
@@ -22,9 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/employees', usersRouter);
-
+app.use('/users', usersRouter); // Users route
+app.use('/employees', employeesRouter); // Employees route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,11 +31,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
